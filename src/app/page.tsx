a5d2354fd9471d99d447f8eb7ce0d2f401349a5e
Gabriel Trejo <gabrielTrejo433@gmail.com>
2025-08-28 23:04:00 -0500

    Refactor HomePage component to use Heading for title and description, improving code readability and component structure.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,16 @@
 import Link from 'next/link';
 
+import Heading from '@/components/Heading';
 import { ticketMainPath } from '@/path';
 
 const HomePage = () => {
   return (
-    <div
-      className="flex flex-col gap-y-8"
-      style={{ animation: 'fade-in-from-top 0.3s ease-out both' }}
-    >
-      <div className="flex-1 flex flex-col">
-        <h2 className="text-3xl font-bold tracking-tight ">Home Page</h2>
-        <p className="text-sm text-muted-foreground">
-          This is the home page of the ticket app.
-        </p>
-      </div>
+    <div className="flex flex-col gap-y-8">
+      <Heading
+        title="Home"
+        description="This is the home page of the ticket app."
+      />
+
       <div className="flex-1 flex flex-col items-center">
         <Link href={ticketMainPath()} className="underline">
           Go to Tickets
